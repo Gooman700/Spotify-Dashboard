48b0a74ec73f00b26d11f2244e3ae7874a4ca49a
Edward <edward@mce123.co.uk>
2025-12-21 12:33:51 +0000

    Imported the tauri sql plugin, and created a "streams" table.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,9 +4,33 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create_stream_table",
+            sql: "CREATE TABLE IF NOT EXISTS streams (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ts TEXT,
+                track_name TEXT,
+                artist_name TEXT,
+                ms_played INTEGER,
+                track_uri TEXT,
+                UNIQUE(ts, track_uri)
+            );",
+            kind: MigrationKind::Up
+        }
+    ];
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:dashboard.db", migrations)
+                .build()
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
